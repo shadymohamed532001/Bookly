87250e916da8db290c5766f421de76db9172f943
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-10-11 17:05:29 +0300

    add Either calss and error Class

--- lib/Features/Home/domain/repos/HomeRepo.dart
+++ lib/Features/Home/domain/repos/HomeRepo.dart
@@ -1,7 +1,8 @@
 import 'package:bookly/Features/Home/domain/entites/bookEntite.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntite>>
+  Future<Either<int, List<BookEntite>>>
       featchFeaturedBook();
 
   Future<List<BookEntite>>
