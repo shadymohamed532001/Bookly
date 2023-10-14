331a588daeca7a3d36cf0f7a648df0cb1db2c05d
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2023-10-14 17:30:01 +0300

    Create HomeRemote DataSources with the newest features

---
+++ lib/Features/Home/data/dataSources/HomeRemoteDataSources/HomeRemoteDataSources.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:bookly/Core/utils/apiServices.dart';
+import 'package:bookly/Features/Home/data/models/book_models/book_models.dart';
+import 'package:bookly/Features/Home/domain/entites/bookEntite.dart';
+
+abstract class HomeRemoteDataSoureces {
+  Future<List<BookEntite>>
+      featchFeaturedBook();
+
+  Future<List<BookEntite>>
+      featchNewsBook();
+}
+
+class HomeRemoteDataSourecesImpl
+    extends HomeRemoteDataSoureces {
+  final ApiServices
+      apiServices;
+
+  HomeRemoteDataSourecesImpl(
+      {required this.apiServices});
+  @override
+  Future<List<BookEntite>>
+      featchFeaturedBook() async {
+    var date =
+        await apiServices.GetServices(endPoint: 'volumes?Filtering=free-ebooks&q=programing');
+
+    List<BookEntite>
+        books =
+        GetBookList(date);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntite>>
+      featchNewsBook() async {
+    var date =
+        await apiServices.GetServices(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest-&q=programing');
+
+    List<BookEntite>
+        books =
+        GetBookList(date);
+    return books;
+  }
+
+  List<BookEntite>
+      GetBookList(Map<String, dynamic> date) {
+    List<BookEntite>
+        books =
+        [];
+
+    for (var bookmap
+        in date['items']) {
+      books.add(BookModels.fromJson(bookmap));
+    }
+    return books;
+  }
+}
